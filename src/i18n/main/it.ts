f7fb75d676aca58bdbb584cea3c045b38dcfda4f
Tom Chen <tomchen.org@gmail.com>
2021-04-05 11:50:47 +0200

    feat: ru (#14); complete Italian i18n (#9)

--- src/i18n/main/it.ts
+++ src/i18n/main/it.ts
@@ -1,57 +1,57 @@
 export const i18n = {
-  tower: 'tower',
-  wall: 'wall',
-  resource: 'resource',
+  tower: 'torre',
+  wall: 'muro',
+  resource: 'risorsa',
 
   bricks: 'mattoni',
   gems: 'gemme',
   recruits: 'reclute',
 
-  quarry: 'quarry', // brick production
-  magic: 'magic', // gem production
+  quarry: 'cava', // brick production
+  magic: 'magia', // gem production
   dungeon: 'dungeon', // recruit production
 
-  discarded: 'Scartata',
-  'Discard a card': 'Getta una carta',
+  discarded: 'scartata',
+  'Discard a card': 'Scartare una carta',
 
-  'You Win!': 'You Win!',
-  'You Lose!': 'You Lose!',
-  'Tie Game': 'Tie Game',
+  'You Win!': 'Tu vinci!',
+  'You Lose!': 'Tu perdi!',
+  'Tie Game': 'Gioco del pareggio',
 
-  Preferences: 'Preferences',
-  'Your Name:': 'Your Name:',
-  "Opponent's Name:": "Opponent's Name:",
+  Preferences: 'Preferenze',
+  'Your Name:': 'Il tuo nome:',
+  "Opponent's Name:": "Nome dell'avversario:",
   'Choose a Tavern (Preset Preferences):':
-    'Choose a Tavern (Preset Preferences):',
-  Default: 'Default',
-  Customized: 'Customized',
-  'Starting Conditions': 'Starting Conditions',
-  'Victory Conditions': 'Victory Conditions',
-  'Other Preferences': 'Other Preferences',
-  'Cards in Hand': 'Cards in Hand',
-  'AI Type': 'AI Type',
-  'Apply & New Game': 'Apply & New Game',
-  Cancel: 'Cancel',
+    'Scegli una taverna (Preferenze preimpostate):',
+  Default: 'Predefinite',
+  Customized: 'Personalizzato',
+  'Starting Conditions': 'Condizioni di partenza',
+  'Victory Conditions': 'Condizioni di vittoria',
+  'Other Preferences': 'Altre preferenze',
+  'Cards in Hand': 'Carte in mano',
+  'AI Type': 'Tipo di IA',
+  'Apply & New Game': 'Applica & Nuova partita',
+  Cancel: 'Annulla',
 
-  Language: 'Language',
+  Language: 'Lingua',
   Volume: 'Volume',
-  Mute: 'Mute',
-  Help: 'Help',
-  'Toggle Full Screen': 'Toggle Full Screen',
+  Mute: 'Muto',
+  Help: 'Aiuto',
+  'Toggle Full Screen': 'Alterna schermo intero',
 
-  ERATHIAN: 'Use Erathian [%s] (for latin letters only)',
+  ERATHIAN: 'Usa Erathian [%s] (solo per lettere latine)',
 
   'ArcoMage HD': 'ArcoMage HD',
 
   DESC:
-    "Web-based open source HD clone of 3DO and NWC's 2000 card game Arcomage",
+    'Clone HD open source basato sul web del gioco di carte Arcomage del 2000 di 3DO e NWC',
 
   'Please go to %s to view more information, star the repo and follow %s there.':
-    'Please go to %s to view more information, star the repo and follow %s there.',
+    'Si prega di andare su %s per vedere maggiori informazioni, startare il repo e seguire %s lì.',
 
-  'the GitHub project page': 'the GitHub project page',
+  'the GitHub project page': 'la pagina del progetto GitHub',
 
-  'Game rules:': 'Game rules:',
+  'Game rules:': 'Regole del gioco:',
 
   GAMERULES: `Le regole per vincere variano di osteria in osteria. Occorre costruire la propria torre, distruggere la torre dell’avversario, o raccogliere abbastanza risorse prima dell’avversario.
 I grandi numeri gialli incolonnati rappresentano i generatori. Si tratta di quante nuove unità di una risorsa particolare riceverete al prossimo turno. I piccoli numeri neri incolonnati sono le risorse. Si tratta di quante unità avete a disposizione durante il turno in corso.
@@ -59,7 +59,7 @@ Carte: il costo di ogni carta è indicato nell’angolo destro inferiore della c
 il rosso rappresenta il Generatore Pietra, che produce i Mattoni; il blu rappresenta il Generatore Magia, che produce le Gemme; il verde rappresenta il Generatore Zoo, che produce gli Animali.`,
 
   'With no usable or discardable card, your opponent has surrendered':
-    'With no usable or discardable card, your opponent has surrendered',
+    'Senza carte utilizzabili o scartate, il tuo avversario si è arreso',
   'With no usable or discardable card, you have surrendered':
-    'With no usable or discardable card, you have surrendered',
+    'Senza carte utilizzabili o scartate, ti sei arreso',
 }
