1e41775d0db3cd7914195e78bc11568ab5e564b7
Tom Chen <tomchen.org@gmail.com>
2021-04-10 00:44:38 +0200

    fix: logo

--- assets/logo/manifest.template.ts
+++ assets/logo/manifest.template.ts
@@ -11,16 +11,16 @@ export default {
   scope: '.',
   background_color: '#000',
   theme_color: '#000',
-  faviconSvgToPngSizes: [36, 72, 96, 120, 128, 144, 152, 180, 192],
-  logoSvgToPngSizes: [384, 512],
-  faviconMaskableSvgSizes: [36, 48, 57, 60, 72, 76, 96, 114, 120, 128, 144, 152, 180, 192, 256],
-  logoMaskableSvgSizes: [384, 512],
+  faviconSvgToPngSizes: [36, 72, 96, 120, 128, 144, 152, 180, 192, 384],
+  logoSvgToPngSizes: [512],
+  faviconMaskableSvgSizes: [36, 48, 57, 60, 72, 76, 96, 114, 120, 128, 144, 152, 180, 192, 256, 384],
+  logoMaskableSvgSizes: [512],
   iconNames: {
     faviconSvg: 'favicon.svg',
     faviconPng: 'favicon-%s.png',
     faviconMaskableSvg: 'favicon_maskable.svg',
-    logoSvg: 'logo.svg',
-    logoPng: 'logo-%s.png',
-    logoMaskableSvg: 'logo_maskable.svg',
+    logoSvg: 'favicon_logo.svg',
+    logoPng: 'favicon_logo-%s.png',
+    logoMaskableSvg: 'favicon_logo_maskable.svg',
   }
 }
