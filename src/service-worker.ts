6de8d329f3220662aa93b516acc3102abf719bd4
Tom Chen <tomchen.org@gmail.com>
2021-04-10 23:40:23 +0200

    fix: pwa mp3 cache (#16)

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,38 +1,23 @@
 declare let self: ServiceWorkerGlobalScope & Window & typeof globalThis
 
-import { skipWaiting, clientsClaim } from 'workbox-core'
-import { precacheAndRoute } from 'workbox-precaching'
-import partition from './utils/partition'
+import { skipWaiting, clientsClaim, cacheNames } from 'workbox-core'
+import { precacheAndRoute, getCacheKeyForURL } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
-import { CacheFirst } from 'workbox-strategies'
-import { CacheableResponsePlugin } from 'workbox-cacheable-response'
+import { CacheOnly } from 'workbox-strategies'
 import { RangeRequestsPlugin } from 'workbox-range-requests'
 
 skipWaiting()
 clientsClaim()
 
+const cacheKeyWillBeUsed: any = ({ request }: { request: any }) =>
+  getCacheKeyForURL(request.url)
+
 registerRoute(
   ({ url }) => url.pathname.endsWith('.mp3'),
-  new CacheFirst({
-    cacheName: 'audio-cache',
-    matchOptions: { ignoreSearch: true },
-    plugins: [
-      new CacheableResponsePlugin({ statuses: [200] }),
-      new RangeRequestsPlugin(),
-    ],
+  new CacheOnly({
+    cacheName: cacheNames.precache,
+    plugins: [{ cacheKeyWillBeUsed }, new RangeRequestsPlugin()],
   }),
 )
 
-type entryType = typeof self.__WB_MANIFEST[0]
-
-const [mp3s, nonMp3s]: [entryType[], entryType[]] = partition(
-  self.__WB_MANIFEST,
-  (entry: entryType) => {
-    if (typeof entry !== 'string') {
-      return entry.url.endsWith('.mp3')
-    }
-    return false
-  },
-)
-
-precacheAndRoute(nonMp3s)
+precacheAndRoute(self.__WB_MANIFEST)
--- src/utils/partition.ts
+++
@@ -1,9 +0,0 @@
-const partition = (array: any[], isValid: (entry: any) => boolean) =>
-  array.reduce(
-    ([pass, fail], elem) => {
-      return isValid(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]]
-    },
-    [[], []],
-  )
-
-export default partition
