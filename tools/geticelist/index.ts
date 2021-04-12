75eb5f06445acf16c554e46458166e14337e0780
Tom Chen <tomchen.org@gmail.com>
2021-04-12 19:32:04 +0200

    feat: get ice server list tool

---
+++ tools/geticelist/index.ts
@@ -0,0 +1,35 @@
+import https from 'https'
+import fs from 'fs'
+
+// get free STUN server list from https://github.com/DamonOehlman/freeice
+
+const url =
+  'https://raw.githubusercontent.com/DamonOehlman/freeice/master/stun.json'
+
+https
+  .get(url, (res) => {
+    let body = ''
+
+    res.on('data', (chunk) => {
+      body += chunk
+    })
+
+    res.on('end', () => {
+      try {
+        let json = JSON.parse(body)
+        const resultJson = JSON.stringify(json, null, 2)
+        fs.writeFileSync(
+          './src/webrtc/icelist.ts',
+          `const icelist = ${resultJson}
+
+export default icelist
+`,
+        )
+      } catch (error) {
+        console.error(error.message)
+      }
+    })
+  })
+  .on('error', (error: Error) => {
+    console.error(error.message)
+  })
