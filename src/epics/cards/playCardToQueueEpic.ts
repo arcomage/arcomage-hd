e8f53247c340f3322a92914bd4fdf40dce3afd94
Tom Chen <tomchen.org@gmail.com>
2021-04-25 12:03:41 +0200

    fix: categorize queues with gameNumber (#27)

--- src/epics/cards/playCardToQueueEpic.ts
+++ src/epics/cards/playCardToQueueEpic.ts
@@ -1,11 +1,17 @@
 import { PLAY_CARD_TO_QUEUE } from '../../constants/ActionTypes'
-import { RootActionType } from '../../types/actionObj'
+import {
+  DiscardCardCoreActionType,
+  RootActionType,
+  UseCardCoreActionType,
+} from '../../types/actionObj'
 import { filter, concatMap } from 'rxjs/operators'
 import { isOfType } from 'typesafe-actions'
 import { ActionsObservable, StateObservable } from 'redux-observable'
 import { RootStateType } from '../../types/state'
 import { EMPTY } from 'rxjs'
-import { playCardQueue as q } from '../../utils/queues'
+import { playCardQueues } from '../../utils/queues'
+import devLog from '../../utils/devLog'
+import Queue from '../../utils/Queue'
 
 export default (
   action$: ActionsObservable<RootActionType>,
@@ -14,8 +20,21 @@ export default (
   action$.pipe(
     filter(isOfType(PLAY_CARD_TO_QUEUE)),
     concatMap((action) => {
-      const { payload } = action
-      q.enqueue(payload)
+      const { payload, gameNumber } = action
+      if (gameNumber === undefined) {
+        devLog(`gameNumber is undefined in ${JSON.stringify(action)}`, 'error')
+        return EMPTY
+      }
+
+      let playCardQueue = playCardQueues.get(gameNumber)
+      if (playCardQueue === undefined) {
+        playCardQueue = new Queue<
+          UseCardCoreActionType | DiscardCardCoreActionType
+        >()
+        playCardQueues.set(gameNumber, playCardQueue)
+      }
+
+      playCardQueue.enqueue(payload)
       return EMPTY
     }),
   )
--- src/epics/multiplayer/receiveEpic.ts
+++ src/epics/multiplayer/receiveEpic.ts
@@ -1,6 +1,6 @@
 import { RECEIVE } from '../../constants/ActionTypes'
 import { RootActionType } from '../../types/actionObj'
-import { filter, concatMap, delay, withLatestFrom } from 'rxjs/operators'
+import { filter, concatMap, delay } from 'rxjs/operators'
 import { concat, EMPTY, of } from 'rxjs'
 import { isOfType } from 'typesafe-actions'
 import { ActionsObservable, StateObservable } from 'redux-observable'
@@ -8,12 +8,11 @@ import { RootStateType } from '../../types/state'
 import {
   ConnDataType,
   instructionActionTypes,
-  verifyGameNumberInstActionTypes,
+  isVerifyGameNumberInst,
 } from '../../types/connData'
 import { INST } from '../../constants/connDataKind'
 import devLog from '../../utils/devLog'
 import { receiveSeq } from '../../utils/seq'
-import { isInArray } from '../../utils/typeHelpers'
 
 export default (
   action$: ActionsObservable<RootActionType>,
@@ -21,9 +20,7 @@ export default (
 ) =>
   action$.pipe(
     filter(isOfType(RECEIVE)),
-    withLatestFrom(state$),
-    concatMap(([action, state]) => {
-      const { gameNumber: curGameNumber } = state.multiplayer
+    concatMap((action) => {
       const { data: connDataStr } = action
       devLog(`received: ${connDataStr}`, 'note')
       try {
@@ -35,31 +32,18 @@ export default (
         }
 
         const ret = usableConnDataList.map((connData) => {
-          const { kind, data, gameNumber: recvGameNumber } = connData
+          const { kind, data, gameNumber } = connData
           switch (kind) {
             case INST: {
-              const { type } = data
-              if (instructionActionTypes.includes(type)) {
-                if (isInArray(type, verifyGameNumberInstActionTypes)) {
-                  if (curGameNumber > recvGameNumber) {
-                    devLog(
-                      `Current gameNumber (game start timestamp) ${curGameNumber} is higher than received in-game instruction ${JSON.stringify(
-                        connData,
-                      )} 's gameNumber. The instruction arriving late is therefore dropped`,
-                      'warning',
-                    )
-                    return EMPTY
-                  } else if (curGameNumber < recvGameNumber) {
-                    devLog(
-                      `Current gameNumber (game start timestamp) ${curGameNumber} is lower than received in-game instruction ${JSON.stringify(
-                        connData,
-                      )} 's gameNumber. The instruction arriving early is pushed to the queue rathen than dropped, but you should be careful`,
-                      'warning',
-                    )
-                  }
+              if (instructionActionTypes.includes(data.type)) {
+                if (isVerifyGameNumberInst(data)) {
+                  return of<RootActionType>({ ...data, gameNumber }).pipe(
+                    delay(0),
+                  )
                 }
                 return of<RootActionType>(data).pipe(delay(0))
               }
+              devLog(`received non-instruction action: ${JSON.stringify(connData)}`, 'error')
               break
             }
           }
