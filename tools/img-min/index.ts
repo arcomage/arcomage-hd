65d240ccad619ebbdee595c6d36fb1e53ac71bef
Tom Chen <tomchen.org@gmail.com>
2021-09-11 23:01:23 +0200

    feat: generate lossless webp images

--- tools/img-min/index.ts
+++ tools/img-min/index.ts
@@ -1,25 +1,28 @@
 ;(async () => {
   const imagemin = (await import('imagemin')).default
   const imageminWebp = (await import('imagemin-webp')).default
-  const imageminPng = (await import('imagemin-optipng')).default
-  await imagemin(['./assets/img/*.png'], {
-    destination: 'assets/img-min',
-    plugins: [
-      imageminWebp({ lossless: true }),
-    ],
+  // const imageminPng = (await import('imagemin-optipng')).default
+  await imagemin(
+    ['./tools/img-min/original/*.png', '!./tools/img-min/original/bg.png'],
+    {
+      destination: './assets/img',
+      plugins: [imageminWebp({ lossless: true })],
+    },
+  )
+  await imagemin(['./tools/img-min/original/bg.png'], {
+    destination: './assets/img',
+    plugins: [imageminWebp({ quality: 90, method: 6, sns: 100 })],
   })
-  await imagemin(['./assets/img/cards/*.png'], {
-    destination: 'assets/img-min/cards',
-    plugins: [
-      imageminWebp({ lossless: true }),
-    ],
+  await imagemin(['./tools/img-min/original/cards/*.png'], {
+    destination: './assets/img/cards',
+    plugins: [imageminWebp({ lossless: true })],
   })
-  await imagemin(['./assets/img/*.png'], {
-    destination: 'assets/img',
-    plugins: [imageminPng({ optimizationLevel: 5 })],
-  })
-  await imagemin(['./assets/img/cards/*.png'], {
-    destination: 'assets/img/cards',
-    plugins: [imageminPng({ optimizationLevel: 5 })],
-  })
+  // await imagemin(['./tools/img-min/original/*.png'], {
+  //   destination: './tools/img-min/opt/',
+  //   plugins: [imageminPng({ optimizationLevel: 5 })],
+  // })
+  // await imagemin(['./tools/img-min/original/cards/*.png'], {
+  //   destination: './tools/img-min/opt/',
+  //   plugins: [imageminPng({ optimizationLevel: 5 })],
+  // })
 })()
