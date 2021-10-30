322d90e2951407475595f12235de6ed61fb2da7f
Tom Chen <tomchen.org@gmail.com>
2021-10-30 22:41:04 +0200

    chore: update material-ui

--- src/components/special/TooltipAll.tsx
+++ src/components/special/TooltipAll.tsx
@@ -1,4 +1,4 @@
-import Tooltip from '@material-ui/core/Tooltip'
+import Tooltip from '@mui/material/Tooltip'
 import React, { memo } from 'react'
 import { tooltipEnterDelay, tooltipLeaveDelay } from '../../constants/visuals'
 
