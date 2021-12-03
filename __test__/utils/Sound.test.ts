a4255c75a432f0cb13912118308156b98f50c368
Tom Chen <tomchen.org@gmail.com>
2021-12-03 19:31:20 +0100

    test: sound

--- __test__/utils/Sound.test.ts
+++ __test__/utils/Sound.test.ts
@@ -1,6 +1,50 @@
-import { Sound } from '../../src/utils/Sound'
+import { setVolume, play } from '../../src/utils/Sound'
+// import { Howl, Howler } from 'howler'
 
-it('Sound is OK', () => {
-  Sound.setVolume(0.5)
-  // Sound.play('brickProd')
+const mockVolumeFn = jest.fn()
+const mockStereoFn = jest.fn()
+const mockPlayFn = jest.fn()
+
+jest.mock('howler', () => {
+  return {
+    Howler: { volume: () => mockVolumeFn() },
+    Howl: jest.fn().mockImplementation(() => {
+      return { stereo: () => mockStereoFn(), play: () => mockPlayFn() }
+    }),
+  }
+})
+
+beforeEach(() => {
+  mockVolumeFn.mockClear()
+  mockStereoFn.mockClear()
+  mockPlayFn.mockClear()
+})
+
+it('setVolume(0.5) is OK', () => {
+  setVolume(0.5)
+  expect(mockVolumeFn).toBeCalledTimes(1)
+})
+
+it('play(`victory`) is OK', () => {
+  play('victory')
+  expect(mockStereoFn).toBeCalledTimes(1)
+  expect(mockPlayFn).toBeCalledTimes(1)
+})
+
+it('play(`deal`, null, 0.8) is OK', () => {
+  play('deal', null, 0.8)
+  expect(mockStereoFn).toBeCalledTimes(1)
+  expect(mockPlayFn).toBeCalledTimes(1)
+})
+
+it('play(`brickProd`, true, true) is OK', () => {
+  play('brickProd', true, true)
+  expect(mockStereoFn).toBeCalledTimes(1)
+  expect(mockPlayFn).toBeCalledTimes(1)
+})
+
+it('play(`gems`, false, false) is OK', () => {
+  play('gems', false, false)
+  expect(mockStereoFn).toBeCalledTimes(1)
+  expect(mockPlayFn).toBeCalledTimes(1)
 })
