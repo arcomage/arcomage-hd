0dbcdba59f26e4c9254893b195a06ba8b6fcc7a0
Tom Chen <tomchen.org@gmail.com>
2021-12-11 10:15:47 +0100

    chore: update dependencies, tailwind conf for 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,24 @@
-const colors = require('tailwindcss/colors')
-const flattenColorPalette =
-  require('tailwindcss/lib/util/flattenColorPalette').default
+// const colors = require('tailwindcss/colors')
+// const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default
+// const defaultTheme = require('tailwindcss/defaultTheme')
 const forms = require('@tailwindcss/forms')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{ts,tsx,js,jsx,scss,sass,css}', './src/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{ts,tsx,js,jsx,scss,sass,css}', './src/index.html'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.coolGray,
-      red: colors.red,
-      blue: colors.blue,
-      green: colors.green,
-      yellow: colors.yellow,
-      darkborder: 'rgba(31, 41, 55, 0.65)',
-      lightborder: 'rgba(243, 244, 246, 0.65)',
-    },
-    zIndex: {
-      0: 0,
-      10: 10,
-      20: 20,
-      30: 30,
-      40: 40,
-      50: 50,
-      60: 60,
-      70: 70,
-      80: 80,
-      90: 90,
-      100: 100,
-      auto: 'auto',
-    },
+    // colors: {
+    //   transparent: 'transparent',
+    //   current: 'currentColor',
+    //   black: colors.black,
+    //   white: colors.white,
+    //   gray: colors.gray,
+    //   red: colors.red,
+    //   blue: colors.blue,
+    //   green: colors.green,
+    //   yellow: colors.yellow,
+    //   darkborder: 'rgba(31, 41, 55, 0.65)',
+    //   lightborder: 'rgba(243, 244, 246, 0.65)',
+    // },
     boxShadow: {
       sm: '0 1px 2px 0 rgba(0, 0, 0, 0.25)',
       DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.5), 0 1px 2px 0 rgba(0, 0, 0, 0.3)',
@@ -46,24 +30,37 @@ module.exports = {
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.30)',
       none: 'none',
     },
-    screens: {
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
+    // screens: {
+    //   sm: '640px',
+    //   // => @media (min-width: 640px) { ... }
 
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
+    //   md: '768px',
+    //   // => @media (min-width: 768px) { ... }
 
-      lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
+    //   lg: '1024px',
+    //   // => @media (min-width: 1024px) { ... }
 
-      xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
+    //   xl: '1280px',
+    //   // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+    //   '2xl': '1536px',
+    //   // => @media (min-width: 1536px) { ... }
+    // },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        darkborder: 'rgba(31, 41, 55, 0.65)',
+        lightborder: 'rgba(243, 244, 246, 0.65)',
+      },
+      zIndex: {
+        60: 60,
+        70: 70,
+        80: 80,
+        90: 90,
+        100: 100,
+      },
     },
-
-    extend: {},
   },
   variants: {
     extend: {
@@ -73,19 +70,19 @@ module.exports = {
 
   plugins: [
     forms,
-    ({ addUtilities, theme, variants }) => {
-      const borderColor = flattenColorPalette(theme('borderColor'))
-      delete borderColor.default
+    // ({ addUtilities, theme, variants }) => {
+    //   const borderColor = flattenColorPalette(theme('borderColor'))
+    //   delete borderColor.default
 
-      const colorMap = Object.keys(borderColor).map((color) => ({
-        [`.border-t-${color}`]: { borderTopColor: borderColor[color] },
-        [`.border-r-${color}`]: { borderRightColor: borderColor[color] },
-        [`.border-b-${color}`]: { borderBottomColor: borderColor[color] },
-        [`.border-l-${color}`]: { borderLeftColor: borderColor[color] },
-      }))
-      const utilities = Object.assign({}, ...colorMap)
+    //   const colorMap = Object.keys(borderColor).map((color) => ({
+    //     [`.border-t-${color}`]: { borderTopColor: borderColor[color] },
+    //     [`.border-r-${color}`]: { borderRightColor: borderColor[color] },
+    //     [`.border-b-${color}`]: { borderBottomColor: borderColor[color] },
+    //     [`.border-l-${color}`]: { borderLeftColor: borderColor[color] },
+    //   }))
+    //   const utilities = Object.assign({}, ...colorMap)
 
-      addUtilities(utilities, variants('borderColor'))
-    },
+    //   addUtilities(utilities, variants('borderColor'))
+    // },
   ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -209,6 +209,7 @@ module.exports = (env, argv) => {
         chunks: 'all',
         maxSize: 20000,
       },
+      runtimeChunk: 'single',
     },
   }
 
