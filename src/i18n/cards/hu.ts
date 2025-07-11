d6d97653b02ce62919c7014bc065e4be7b6ad1cd
gbq6 <24852447+gbq6@users.noreply.github.com>
2025-07-11 17:09:39 +0200

    Hungarian translation

---
+++ src/i18n/cards/hu.ts
@@ -0,0 +1,412 @@
+import { DataCardsI18nType } from '../types'
+
+export const cardsI18n: DataCardsI18nType = [
+  {
+    name: 'Téglahiány',
+    desc: 'Minden játékos veszít 8 téglát',
+  },
+  {
+    name: 'Szerencsés lelet',
+    desc: '+2 tégla, +2 drágakő. Újra te jössz',
+  },
+  {
+    name: 'Barátságos terep',
+    desc: '+1 fal. Újra te jössz',
+  },
+  {
+    name: 'Bányászok',
+    desc: '+1 bánya',
+  },
+  {
+    name: 'Aranybánya',
+    desc: 'Ha bánya < ellenséges bánya, +2 bánya, különben +1 bánya',
+  },
+  {
+    name: 'Törpe bányászok',
+    desc: '+4 fal, +1 bánya',
+  },
+  {
+    name: 'Túlóra',
+    desc: '+5 fal. Veszítesz 6 drágakövet',
+  },
+  {
+    name: 'Másolt technológia',
+    desc: 'Ha bánya < ellenséges bánya, bánya = ellenséges bánya',
+  },
+  {
+    name: 'Alapfal',
+    desc: '+3 fal',
+  },
+  {
+    name: 'Masszív fal',
+    desc: '+4 fal',
+  },
+  {
+    name: 'Újítások',
+    desc: "+1 bánya minden játékosnak, kapsz 4 drágakövet",
+  },
+  {
+    name: 'Alapozás',
+    desc: 'Ha fal = 0, +6 fal, különben +3 fal',
+  },
+  {
+    name: 'Rengések',
+    desc: 'Minden fal kap 5 sebzést. Újra te jössz',
+  },
+  {
+    name: 'Titkos szoba',
+    desc: '+1 mágia. Újra te jössz',
+  },
+  {
+    name: 'Földrengés',
+    desc: "-1 bánya minden játékosnak",
+  },
+  {
+    name: 'Nagy fal',
+    desc: '+6 fal',
+  },
+  {
+    name: 'Összeomlás!',
+    desc: '-1 ellenséges bánya',
+  },
+  {
+    name: 'Új felszerelés',
+    desc: '+2 bánya',
+  },
+  {
+    name: 'Külszíni fejtés',
+    desc: '-1 bánya, +10 fal. Kapsz 5 drágakövet',
+  },
+  {
+    name: 'Megerősített fal',
+    desc: '+8 fal',
+  },
+  {
+    name: 'Rostély',
+    desc: '+5 fal, +1 állatkert',
+  },
+  {
+    name: 'Kristálykövek',
+    desc: '+7 fal, kapsz 7 drágakövet',
+  },
+  {
+    name: 'Harmónikus érc',
+    desc: '+6 fal, +3 torony',
+  },
+  {
+    name: 'Óriásfal',
+    desc: '+12 fal',
+  },
+  {
+    name: 'Precíz tervezés',
+    desc: '+8 fal, +5 torony',
+  },
+  {
+    name: 'Hatalmas fal',
+    desc: '+15 fal',
+  },
+  {
+    name: 'Kővető',
+    desc: '+6 fal. 10 sebzés az ellenségnek',
+  },
+  {
+    name: "Sárkányszív",
+    desc: '+20 fal, +8 torony',
+  },
+  {
+    name: 'Kényszermunka',
+    desc: '+9 fal, veszítesz 5 szörnyet',
+  },
+  {
+    name: 'Kőkert',
+    desc: '+1 fal, +1 torony, +2 szörny',
+  },
+  {
+    name: 'Árvíz',
+    desc: 'A legkisebb fallal rendelkező játékos(ok) -1 állatkertet és 2 sebzést kap(nak).',
+  },
+  {
+    name: 'Laktanya',
+    desc: '+6 szörny, +6 fal. Ha állatkert < ellenséges állatkert, +1 állatkert',
+  },
+  {
+    name: 'Bástyák',
+    desc: '+7 fal, 6 sebzés az ellenségnek',
+  },
+  {
+    name: 'Csere',
+    desc: 'Cseréld fel a falad az ellenség falával',
+  },
+  {
+    name: 'Kvarc',
+    desc: '+1 torony. Újra te jössz',
+  },
+  {
+    name: 'Füstkvarc',
+    desc: '1 sebzés az ellenséges toronynak. Újra te jössz',
+  },
+  {
+    name: 'Ametiszt',
+    desc: '+3 torony',
+  },
+  {
+    name: 'Varázsszövők',
+    desc: '+1 mágia',
+  },
+  {
+    name: 'Prizma',
+    desc: 'Húzz 1 kártyát. Dobj 1 kártyát. Újra te jössz',
+  },
+  {
+    name: 'Mágneskő',
+    desc: "+3 torony. Ezt a kártyát nem lehet eldobni",
+  },
+  {
+    name: 'Napkitörés',
+    desc: '+2 torony. 2 sebzés az ellenséges toronynak',
+  },
+  {
+    name: 'Kristálymátrix',
+    desc: '+1 mágia, +3 torony, +1 ellenséges torony',
+  },
+  {
+    name: 'Ékkőhiba',
+    desc: '3 sebzés az ellenséges toronynak',
+  },
+  {
+    name: 'Rubin',
+    desc: '+5 torony',
+  },
+  {
+    name: 'Drágakőlándzsa',
+    desc: '5 sebzés az ellenséges toronynak',
+  },
+  {
+    name: 'Teljes erőbedobás',
+    desc: '5 sebzés a tornyodnak, +2 mágia',
+  },
+  {
+    name: 'Harmónikus rezgés',
+    desc: '+1 mágia, +3 torony, +3 fal',
+  },
+  {
+    name: 'Egyenlőség',
+    desc: "Minden játékos mágiája egyenlő a legnagyobb mágiával",
+  },
+  {
+    name: 'Smaragd',
+    desc: '+8 torony',
+  },
+  {
+    name: 'Bölcsesség gyöngye',
+    desc: '+5 torony, +1 mágia',
+  },
+  {
+    name: 'Zúzó',
+    desc: '-1 mágia. 9 sebzés az ellenséges toronynak',
+  },
+  {
+    name: 'Morzsolókő',
+    desc: '+5 torony. Az ellenség veszít 6 téglát',
+  },
+  {
+    name: 'Zafír',
+    desc: '+11 torony',
+  },
+  {
+    name: 'Viszály',
+    desc: '7 sebzés minden toronynak, minden játékosnak -1 mágia',
+  },
+  {
+    name: 'Tűzrubin',
+    desc: '+6 torony. 4 sebzés az ellenséges toronynak',
+  },
+  {
+    name: "A bánya segítsége",
+    desc: '+7 torony. Veszítesz 10 téglát',
+  },
+  {
+    name: 'Kristálypajzs',
+    desc: '+8 torony, +3 fal',
+  },
+  {
+    name: 'Együttérzés ékköve',
+    desc: '+8 torony, +1 állatkert',
+  },
+  {
+    name: 'Gyémánt',
+    desc: '+15 torony',
+  },
+  {
+    name: 'Menedék',
+    desc: '+10 torony, +5 fal, kapsz 5 szörnyet',
+  },
+  {
+    name: 'Lávakő',
+    desc: '+12 torony. 6 sebzés az ellenségnek',
+  },
+  {
+    name: "Sárkányszem",
+    desc: '+20 torony',
+  },
+  {
+    name: 'Kristályosítás',
+    desc: '+11 torony, -6 fal',
+  },
+  {
+    name: 'Csecsebecsecsomag',
+    desc: 'Ha torony < ellenséges torony, +2 torony, különben +1 torony',
+  },
+  {
+    name: 'Szivárvány',
+    desc: '+1 torony minden játékosnak. Kapsz 3 drágakövet',
+  },
+  {
+    name: 'Inas',
+    desc: '+4 torony, veszítesz 3 szörnyet, 2 sebzés az ellenséges toronynak',
+  },
+  {
+    name: 'Villámszilánk',
+    desc: 'Ha torony > ellenséges fal, 8 sebzés az ellenséges toronynak, különben 8 sebzés',
+  },
+  {
+    name: 'Fázisékkő',
+    desc: '+13 torony, +6 szörny, +6 tégla',
+  },
+  {
+    name: 'Kergemarhakór',
+    desc: 'Minden játékos veszít 6 szörnyet',
+  },
+  {
+    name: 'Tündér',
+    desc: '2 sebzés. Újra te jössz',
+  },
+  {
+    name: 'Szeszélyes koboldok',
+    desc: '4 sebzés. Veszítesz 3 drágakövet',
+  },
+  {
+    name: 'Minotaurusz',
+    desc: '+1 állatkert',
+  },
+  {
+    name: 'Tünde felderítő',
+    desc: 'Húzz 1 kártyát. Dobj 1 kártyát. Újra te jössz',
+  },
+  {
+    name: 'Koboldhorda',
+    desc: '6 sebzés. Elszenvedsz 3 sebzést',
+  },
+  {
+    name: 'Kobold íjászok',
+    desc: '3 sebzés az ellenséges toronynak. Elszenvedsz 1 sebzést',
+  },
+  {
+    name: 'Árnytündér',
+    desc: '2 sebzés az ellenséges toronynak. Újra te jössz',
+  },
+  {
+    name: 'Ork',
+    desc: '5 sebzés',
+  },
+  {
+    name: 'Törpék',
+    desc: '4 sebzés, +3 fal',
+  },
+  {
+    name: 'Kis kígyók',
+    desc: '4 sebzés az ellenséges toronynak',
+  },
+  {
+    name: 'Trollidomár',
+    desc: '+2 állatkert',
+  },
+  {
+    name: 'Toronyszörnyecske',
+    desc: '2 sebzés, +4 torony, +2 fal',
+  },
+  {
+    name: 'Telihold',
+    desc: "+1 állatkert minden játékosnak. Kapsz 3 szörnyet",
+  },
+  {
+    name: 'Hasító',
+    desc: '6 sebzés',
+  },
+  {
+    name: 'Ogre',
+    desc: '7 sebzés',
+  },
+  {
+    name: 'Veszett bárány',
+    desc: '6 sebzés. Az ellenség veszít 3 szörnyet',
+  },
+  {
+    name: 'Ördögfióka',
+    desc: '6 sebzés. Minden játékos veszít 5 téglát, drágakövet és szörnyet',
+  },
+  {
+    name: 'Spizzer',
+    desc: 'Ha ellenséges fal = 0, 10 sebzés, különben 6 sebzés',
+  },
+  {
+    name: 'Vérfarkas',
+    desc: '9 sebzés',
+  },
+  {
+    name: 'Korróziós felhő',
+    desc: 'Ha ellenséges fal > 0, 10 sebzés, különben 7 sebzés',
+  },
+  {
+    name: 'Egyszarvú',
+    desc: 'Ha mágia > ellenséges mágia, 12 sebzés, különben 8 sebzés',
+  },
+  {
+    name: 'Tünde íjászok',
+    desc: 'Ha fal > ellenséges fal, 6 sebzés az ellenséges toronynak, különben 6 sebzés',
+  },
+  {
+    name: 'Succubus',
+    desc: '5 sebzés az ellenséges toronynak, az ellenség veszít 8 szörnyet',
+  },
+  {
+    name: 'Kőtaposók',
+    desc: '8 sebzés, -1 ellenséges bánya',
+  },
+  {
+    name: 'Tolvaj',
+    desc: 'Az ellenség veszít 10 drágakövet, 5 téglát, a felét megkapod (felkerekítve)',
+  },
+  {
+    name: 'Kőóriás',
+    desc: '10 sebzés, +4 fal',
+  },
+  {
+    name: 'Vámpír',
+    desc: '10 sebzés. Az ellenség veszít 5 szörnyet, -1 ellenséges állatkert',
+  },
+  {
+    name: 'Sárkány',
+    desc: '20 sebzés. Az ellenség veszít 10 drágakövet, -1 ellenséges állatkert',
+  },
+  {
+    name: 'Lándzsás',
+    desc: 'Ha fal > ellenséges fal, 3 sebzés, különben 2 sebzés',
+  },
+  {
+    name: 'Gnóm',
+    desc: '3 sebzés, +1 drágakő',
+  },
+  {
+    name: 'Vérengző',
+    desc: '8 sebzés. 3 sebzés a tornyodnak',
+  },
+  {
+    name: 'Hadúr',
+    desc: '13 sebzés. Veszítesz 3 drágakövet',
+  },
+  {
+    name: 'Pegazus lándzsás',
+    desc: '12 sebzés az ellenséges toronynak',
+  },
+]
---
+++ src/i18n/taverns/hu.ts
@@ -0,0 +1,124 @@
+import { TavernsI18nType } from '../types'
+
+export const tavernsI18n: TavernsI18nType = [
+  {
+    name: 'A Ház Ajándéka',
+    location: 'Harmondale',
+  },
+  {
+    name: "A Griff Pihenője",
+    location: 'Erathia',
+  },
+  {
+    name: 'Smaragd Fogadó',
+    location: 'Tularean-erdő',
+  },
+  {
+    name: 'A Sznob Kobold',
+    location: 'Deyja',
+  },
+  {
+    name: 'Ismerős Hely',
+    location: 'Bracada-sivatag',
+  },
+  {
+    name: 'Az Áldott Főzet',
+    location: 'Celeste',
+  },
+  {
+    name: 'A Vampírbár',
+    location: 'A pokol',
+  },
+  {
+    name: 'A Nevető Szerzetes',
+    location: 'Evenmorn-sziget',
+  },
+  {
+    name: "Szeszélyes Szerencse",
+    location: 'Nighon-hegy',
+  },
+  {
+    name: "Bányászoknak Fenntartva",
+    location: 'Barrow-dombság',
+  },
+  {
+    name: 'A Hű Zsoldos',
+    location: 'Tatalia',
+  },
+  {
+    name: 'Cserepes Tündér',
+    location: 'Avlee',
+  },
+  {
+    name: "Grogg Grogja",
+    location: 'Kőváros',
+  },
+  {
+    name: 'Grog és Kaja',
+    location: 'Tőrseb-sziget',
+  },
+  {
+    name: "Kessel Kantinja",
+    location: 'Hollópart',
+  },
+  {
+    name: 'Haszonház',
+    location: 'Alvar',
+  },
+  {
+    name: 'Kiszáradt Torok',
+    location: 'Vaspor-sivatag',
+  },
+  {
+    name: "Sárkány Vére Fogadó",
+    location: 'Garrote-szurdok',
+  },
+  {
+    name: 'Fekete Társaság',
+    location: 'Árnycsúcs',
+  },
+  {
+    name: "Az Utazó Pihenője",
+    location: 'Mormolóerdő',
+  },
+  {
+    name: "Telitalálat Fogadó",
+    location: 'Vándorvadon',
+  },
+  {
+    name: "A Kalóz Pihenője",
+    location: 'Regna',
+  },
+  {
+    name: 'A Táncoló Ogre',
+    location: 'Hollópart',
+  },
+  {
+    name: "Miho Csárdája",
+    location: 'Alvar',
+  },
+  {
+    name: 'Lord Osric Temper',
+    location: 'Szabad Menedék',
+  },
+  {
+    name: 'Lord Anthony Stone',
+    location: 'Fagyos Felföldek',
+  },
+  {
+    name: 'Lord Erik von Stromgard',
+    location: 'Fagyos Felföldek',
+  },
+  {
+    name: 'Lady Loretta Fleise',
+    location: 'Ezüst-öböl',
+  },
+  {
+    name: 'Lord Albert Newton',
+    location: 'Köd-szigetek',
+  },
+  {
+    name: 'Nicolai Ironfist herceg',
+    location: 'Vasököl-kastély',
+  },
+]
